cffacba3ac85f9b7d30e08141c2555821e0235a4
Sarah Steiner <steiner.sa96@gmail.com>
2019-02-28 16:01:16 -0600

    If statements comparing gestures complete. Still need conditional for a tie. Still need to add user input validation.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -104,6 +104,51 @@ namespace RPSLS
                 Console.WriteLine("Spock smashes scissors! Round point goes to {0}", player2.name);
                 player2.score++;
             }
+            if (player1.gesture == "scissors" && player2.gesture == "lizard")
+            {
+                Console.WriteLine("Scissors decapitates lizard! Round point goes to {0}", player1.name);
+                player1.score++;
+            }
+            if (player2.gesture == "scissors" && player1.gesture == "lizard")
+            {
+                Console.WriteLine("Scissors decapitates lizard! Round point goes to {0}", player2.name);
+                player2.score++;
+            }
+            if (player1.gesture == "lizard" && player2.gesture == "paper")
+            {
+                Console.WriteLine("Lizard eats paper! Round point goes to {0}", player1.name);
+                player1.score++;
+            }
+            if (player2.gesture == "lizard" && player1.gesture == "paper")
+            {
+                Console.WriteLine("Lizard eats paper! Round point goes to {0}", player2.name);
+                player2.score++;
+            }
+            if (player1.gesture == "paper" && player2.gesture == "spock")
+            {
+                Console.WriteLine("Paper disproves spock! Round point goes to {0}", player1.name);
+                player1.score++;
+            }
+            if (player2.gesture == "paper" && player1.gesture == "spock")
+            {
+                Console.WriteLine("Paper disproves spock! Round point goes to {0}", player2.name);
+                player2.score++;
+            }
+            if (player1.gesture == "spock" && player2.gesture == "rock")
+            {
+                Console.WriteLine("Spock vaporizes rock! Round point goes to {0}", player1.name);
+                player1.score++;
+            }
+            if (player2.gesture == "spock" && player1.gesture == "rock")
+            {
+                Console.WriteLine("Spock vaporizes rock! Round point goes to {0}", player2.name);
+                player2.score++;
+            }
+            if (player1.gesture == player2.gesture)
+            {
+                Console.WriteLine("It's a tie! Try again.");
+                
+            }
         }
 
     }
