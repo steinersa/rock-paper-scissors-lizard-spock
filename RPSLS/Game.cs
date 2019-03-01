46c67bbcb5cd2ae540a8d1d91a5f1c6fff9eb8a9
Sarah Steiner <steiner.sa96@gmail.com>
2019-03-01 07:52:32 -0600

    Player score is incremented if they win round. Method to check if there is a game winner working. If there is, game ends. Working on game flow if there is not a winner.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -21,6 +21,13 @@ namespace RPSLS
         {
             ChooseHowManyPlayers();
             CompareGestures();
+            NoteToPlayer();
+            CheckIfGameWinner();
+            MakeChoices();
+            CompareGestures();
+            NoteToPlayer();
+            CheckIfGameWinner();
+
             //if score is less than 2
             //go back to choosegesture under player
             //else say who won
@@ -46,102 +53,102 @@ namespace RPSLS
         {
             if (player1.gesture == "rock" && player2.gesture == "scissors")
             {
-                Console.WriteLine("Rock crushes scissors! Round point goes to {0}", player1.name);
+                Console.WriteLine("Rock crushes scissors! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "rock" && player1.gesture == "scissors")
             {
-                Console.WriteLine("Rock crushes scissors! Round point goes to {0}", player2.name);
+                Console.WriteLine("Rock crushes scissors! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "scissors" && player2.gesture == "paper")
             {
-                Console.WriteLine("Scissors cuts paper! Round point goes to {0}", player1.name);
+                Console.WriteLine("Scissors cuts paper! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "scissors" && player1.gesture == "paper")
             {
-                Console.WriteLine("Scissors cuts paper! Round point goes to {0}", player2.name);
+                Console.WriteLine("Scissors cuts paper! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "paper" && player2.gesture == "rock")
             {
-                Console.WriteLine("Paper covers rock! Round point goes to {0}", player1.name);
+                Console.WriteLine("Paper covers rock! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "paper" && player1.gesture == "rock")
             {
-                Console.WriteLine("Paper covers rock! Round point goes to {0}", player2.name);
+                Console.WriteLine("Paper covers rock! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "rock" && player2.gesture == "lizard")
             {
-                Console.WriteLine("Rock crushes lizard! Round point goes to {0)", player1.name);
+                Console.WriteLine("Rock crushes lizard! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "rock" && player1.gesture == "lizard")
             {
-                Console.WriteLine("Rock crushes lizard! Round point goes to {0)", player2.name);
+                Console.WriteLine("Rock crushes lizard! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "lizard" && player2.gesture == "spock")
             {
-                Console.WriteLine("Lizard poisons spock! Round point goes to {0)", player1.name);
+                Console.WriteLine("Lizard poisons spock! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "lizard" && player1.gesture == "spock")
             {
-                Console.WriteLine("Lizard poisons spock! Round point goes to {0)", player2.name);
+                Console.WriteLine("Lizard poisons spock! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "spock" && player2.gesture == "scissors")
             {
-                Console.WriteLine("Spock smashes scissors! Round point goes to {0}", player1.name);
+                Console.WriteLine("Spock smashes scissors! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "spock" && player1.gesture == "scissors")
             {
-                Console.WriteLine("Spock smashes scissors! Round point goes to {0}", player2.name);
+                Console.WriteLine("Spock smashes scissors! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "scissors" && player2.gesture == "lizard")
             {
-                Console.WriteLine("Scissors decapitates lizard! Round point goes to {0}", player1.name);
+                Console.WriteLine("Scissors decapitates lizard! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "scissors" && player1.gesture == "lizard")
             {
-                Console.WriteLine("Scissors decapitates lizard! Round point goes to {0}", player2.name);
+                Console.WriteLine("Scissors decapitates lizard! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "lizard" && player2.gesture == "paper")
             {
-                Console.WriteLine("Lizard eats paper! Round point goes to {0}", player1.name);
+                Console.WriteLine("Lizard eats paper! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "lizard" && player1.gesture == "paper")
             {
-                Console.WriteLine("Lizard eats paper! Round point goes to {0}", player2.name);
+                Console.WriteLine("Lizard eats paper! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "paper" && player2.gesture == "spock")
             {
-                Console.WriteLine("Paper disproves spock! Round point goes to {0}", player1.name);
+                Console.WriteLine("Paper disproves spock! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "paper" && player1.gesture == "spock")
             {
-                Console.WriteLine("Paper disproves spock! Round point goes to {0}", player2.name);
+                Console.WriteLine("Paper disproves spock! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == "spock" && player2.gesture == "rock")
             {
-                Console.WriteLine("Spock vaporizes rock! Round point goes to {0}", player1.name);
+                Console.WriteLine("Spock vaporizes rock! Round point goes to {0}!", player1.name);
                 player1.score++;
             }
             if (player2.gesture == "spock" && player1.gesture == "rock")
             {
-                Console.WriteLine("Spock vaporizes rock! Round point goes to {0}", player2.name);
+                Console.WriteLine("Spock vaporizes rock! Round point goes to {0}!", player2.name);
                 player2.score++;
             }
             if (player1.gesture == player2.gesture)
@@ -149,6 +156,42 @@ namespace RPSLS
                 Console.WriteLine("It's a tie! Try again.");
                 
             }
+        }
+
+        public void CheckIfGameWinner()
+        {
+            //put loop in here if need to choosegesture again
+            if (player1.score == 2)
+            {
+                Console.WriteLine("CONGRATS {0}! You are the game champion! Press any key to exit.", player1.name);
+                Console.ReadLine();
+            }
+            if (player2.score == 2)
+            {
+                Console.WriteLine("CONGRATS {0}! You are the game champion! Press any key to exit.", player2.name);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Next round!");
+                MakeChoices();
+            }
+        }
+
+        public void MakeChoices()
+        {
+            player1.ChooseGesture();
+            player2.ChooseGesture();
+        }
+
+        public void RestartGame()
+        {
+
+        }
+
+        public void NoteToPlayer()
+        {
+            Console.ReadLine();
         }
 
     }
